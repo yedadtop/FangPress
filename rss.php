9faf28aae2f83c338f51e4db2c561b8699effa84
pad <yedad@qq.com>
2026-02-01 15:49:54 +0000

    Update: 2026-02-01 23:49:53

---
+++ rss.php
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title><![CDATA[点滴记忆]]></title> 
+<description><![CDATA[使用emlog搭建的站点]]></description>
+<link>http://10.0.0.8:6/</link>
+<language>zh-cn</language>
+<generator>www.emlog.net</generator>
+<item>
+    <title>未走的路</title>
+    <link>http://10.0.0.8:6/?post=3</link>
+    <description><![CDATA[<span style="font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;深秋的林间，落叶铺就两条蜿蜒的小径，一条开阔平坦，印着往来行人的足迹；一条幽深静谧，藏在枝叶的阴影里，唯有风声掠过枝头，仿佛在诉说着无人探寻的秘密。站在路口，我想起弗罗斯特的诗句，原来人生处处都是这样的岔路口，我们选择其中一条，便注定要与另一条未走的路，隔着岁月遥遥相望。</span><br style="margin:0px;padding:0px;box-sizing:border-box;font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;" />
+<span style="font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;">&nbsp; &nbsp; &nbsp; &nbsp; 年少时的选择，总带着几分莽撞与憧憬。中考结束后，我站在重点高中与艺术附中的岔路口，一边是众人期许的安稳前程，一边是我藏在心底的绘画梦想。最终，我握着重点高中的录取通知书，转身告别了那间摆满画具的小屋。往后的日子里，我在题海中奋力前行，偶尔路过美术馆，看着玻璃窗内的画作，总会想起那条未走的路——那里或许有颜料的芬芳，有画笔在画布上滑动的声响，有我未曾实现的另一种模样。</span><br style="margin:0px;padding:0px;box-sizing:border-box;font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;" />
+<span style="font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;">&nbsp;&nbsp;&nbsp;&nbsp;我们总在选择之后，不自觉地美化那条未走的路。就像林间的小径，我们走在平坦的路上，会羡慕另一条路上的清幽；走在幽深的路上，又会向往另一条路上的热闹。我曾无数次设想，若是当年选择了艺术附中，如今的我会不会正坐在画室里，描绘着日出日落，把生活过成自己喜欢的画卷？会不会没有应试的压力，只有热爱与自由相伴？可我也清楚，那些美好的设想，不过是岁月赋予未走之路的滤镜，褪去滤镜，那条路上或许也有迷茫与艰辛，有挫折与彷徨。</span><br style="margin:0px;padding:0px;box-sizing:border-box;font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;" />
+<span style="font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;">&nbsp;&nbsp;&nbsp;&nbsp;渐渐长大，我才明白，未走的路，从来都不是用来遗憾的，而是用来照亮当下的。那条被我放弃的绘画之路，虽未踏足，却始终在我心底发光。我会在周末抽出时间练习画画，把生活中的美好与失意都藏进画作里，那些未实现的热爱，终究以另一种方式陪伴着我。而我选择的求学之路，也让我收获了知识与成长，遇见了志同道合的朋友，学会了坚持与担当。</span><br style="margin:0px;padding:0px;box-sizing:border-box;font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;" />
+<span style="font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;">&nbsp;&nbsp;&nbsp;&nbsp;如今再回望那个深秋的林间路口，我不再为未走的路耿耿于怀。人生就像一场单程旅行，每一条路都有它独有的风景，每一次选择都藏着它既定的意义。未走的路，是心底的念想，是成长的印记，它提醒我们，要珍惜当下所走的每一步，认真对待每一次选择，因为每一条被坚定走下去的路，终会通往属于自己的繁花似锦。</span><br style="margin:0px;padding:0px;box-sizing:border-box;font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;" />
+<span style="font-family:-apple-system, BlinkMacSystemFont, &quot;font-size:16px;text-wrap-mode:wrap;background-color:#FFFFFF;">&nbsp;&nbsp;&nbsp;&nbsp;或许，我们永远都会对未走的路心怀好奇，但正是这份好奇，让我们在当下的路上更加坚定。毕竟，人生没有白走的路，每一步都算数，而那些未走的路，终将成为我们生命中最温柔的念想，照亮我们前行的方向。</span>]]></description>
+    <pubDate>Sun, 01 Feb 2026 13:47:41 +0000</pubDate>
+    <author>yedad</author>
+    <guid>http://10.0.0.8:6/?post=3</guid>
+
+</item>
+<item>
+    <title>你好</title>
+    <link>http://10.0.0.8:6/?post=2</link>
+    <description><![CDATA[&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 你好<br />
+今天填补本次聚会几乎是不·1不变愈发突出v丰田车队v给<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 哈哈哈<br />
+而无法·<br />
+&nbsp; &nbsp; 去问问<br />
+&nbsp; &nbsp; &nbsp; &nbsp; 收到v给娃儿基本不变和v旅改称v刚才v滚粗1因此v路过]]></description>
+    <pubDate>Sun, 01 Feb 2026 13:45:20 +0000</pubDate>
+    <author>yedad</author>
+    <guid>http://10.0.0.8:6/?post=2</guid>
+
+</item>
+<item>
+    <title>欢迎使用emlog</title>
+    <link>http://10.0.0.8:6/?post=1</link>
+    <description><![CDATA[恭喜您成功安装了emlog，这是系统自动生成的演示文章。编辑或者删除它，然后开始您的创作吧！]]></description>
+    <pubDate>Sun, 01 Feb 2026 13:44:22 +0000</pubDate>
+    <author>yedad</author>
+    <guid>http://10.0.0.8:6/?post=1</guid>
+
+</item></channel>
+</rss>
